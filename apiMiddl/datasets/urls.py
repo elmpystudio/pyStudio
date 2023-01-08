e43899d6c84f0c39256d601c607121de144b97c3
Jameel Hashim <31222266+Jameel-Hashim@users.noreply.github.com>
2023-01-08 11:22:04 +0300

    Dev 2023 (#34)
    * update dev (#29)
    
    * Update JsplumbClient.js
    
    after updating service layer some old tags need to be changed
    
    * Update NodeManager.js
    
    after updating API; URL refactor and no need to have parsing as we are already getting a JSON
    
    * upload marketplace GIF
    
    * update (#30)
    
    * Update JsplumbClient.js
    
    after updating service layer some old tags need to be changed
    
    * Update NodeManager.js
    
    after updating API; URL refactor and no need to have parsing as we are already getting a JSON
    
    * Update README.md
    
    some use cases examples
    
    * Add files via upload
    
    dataset gif example
    
    * Update README.md
    
    * Update README.md
    
    * Add files via upload
    
    ml studio example
    
    * update README
    
    * update README
    
    * update README
    
    * update README
    
    * update
    *rename services app to ml_model.
    *connect markertplace to ml_models.
    *fix markerplace
    
    * update:
    *API* add ml_model delete.
    *Front-end* add ml_models to marketplace.
    *Front-end* fix CCard component.
    
    * update:
    * Fix DatasetRead input in ML.
    * Fix Datasets Table.
    
    Co-authored-by: Enrique Gomicia-Murcia <112491391+egomiciamur@users.noreply.github.com>
    Co-authored-by: jameel_8 <jameelhashim.official@yahoo.com>
    Co-authored-by: egomicia <enrique.gomicia@gmail.com>

--- apiMiddl/datasets/urls.py
+++ apiMiddl/datasets/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
         path('', views.DatasetList.as_view()),
-        path('<int:pk>', views.DatasetDetail.as_view()),
         path('sample/<int:pk>', views.DatasetSampleView.as_view()),
         path('raport/html/<int:pk>', views.DatasetRaportHtmlView.as_view()),
         path('raport/json/<int:pk>', views.DatasetRaportJsonView.as_view()),
+        path('<int:pk>', views.DatasetDetail.as_view()),
     ]
--- apiMiddl/ml_models/urls.py
+++ apiMiddl/ml_models/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.Ml_modelsList.as_view()),
     path("uploadcsv", views.uploadcsv),
     path("run", views.run),
+    path("<pk>", views.Ml_modelsDetail.as_view()),
 ]
