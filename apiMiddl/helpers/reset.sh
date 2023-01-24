32d5cb3d40f3dc8d687f5ce589cf3ac4cc4b3469
jameel_8 <jameelhashim.official@yahoo.com>
2023-01-24 09:05:28 +0300

    Jupyterhub: Upload Datasets into the container.

--- apiMiddl/helpers/reset.sh
+++ apiMiddl/helpers/reset.sh
@@ -2,10 +2,9 @@
 
 rm -rf */migrations/0*.py 
 rm -f ./db.sqlite3
-cp ./helpers/settings.py.backup ./rest/settings.py
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata */fixtures/*
 
-# For Marketplace (Download the files from Minio, then pass it throw)
-mkdir -p ./media/tmp
+# Marketplace downloads tmp folder
+mkdir -p /tmp/api/jupyterhub
